6a3259cc473ecc0575dfb3ef05b18bf38db5b45e
Developer Hope <me@devhope.co>
2020-05-20 13:59:47 +0000

    Remove the commas from table

--- main.js
+++ main.js
@@ -59,6 +59,9 @@ function writeToDocument(url) {
       tableRows.push(`<tr>${dataRow}</tr>`);
     });
 
-    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(
+      /,/g,
+      ""
+    );
   });
 }
