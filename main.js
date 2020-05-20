10fd3023dc51fb1be88ba07852747099d6c43779
Developer Hope <me@devhope.co>
2020-05-20 08:46:07 +0000

    Implemented callback to log data properly

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var xhr = new XMLHttpRequest();
-var data;
+function getData(cb) {
+  var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.send();
 
-function setData(jsonData) {
-  data = jsonData;
-  console.log(data);
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
 }
 
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    setData(JSON.parse(this.responseText));
-  }
-};
+getData(function (data) {
+  console.log(data);
+});
